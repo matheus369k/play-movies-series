6ce269973a26c5eb89362220a924183efd36e4c4
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-05-08 19:21:21 -0300

    add font

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,9 @@
+import { Header } from "./components/header/header";
+
 export function App() {
   return (
-    <div className="bg-black min-h-screen">
-      
+    <div className="bg-black min-h-screen font-inter">
+      <Header />
     </div>
   )
 }
