464dc4e3644222754ed7251b8f8aa0f4689ef5e7
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-05-09 19:13:23 -0300

    update focus movies in home component

--- src/components/pages/home/components/focus-movies.tsx
+++ src/components/pages/home/components/focus-movies.tsx
@@ -1,6 +1,8 @@
 import axios from "axios"
 import { FaPlay } from "react-icons/fa";
 import { useEffect, useState } from "react"
+import { GrNext } from "react-icons/gr";
+import { GrPrevious } from "react-icons/gr";
 
 interface FMoviesInFocus {
     id: number
@@ -19,27 +21,44 @@ export function MoviesInFocus() {
     }, [])
 
     return (
-        <>
+        <section className="bg-focus-movie">
             {movies &&
                 movies.map((movie) => (
-                    <section className={`w-full min-h-screen bg-focus-movie bg-cover flex items-center flex-col gap-10 justify-end pb-10`}>
-                        <div key={movie.id} className="flex items-center flex-col gap-10">
-                            <img src={movie.capa} className="w-44 rounded-md border border-gray-100" />
+                    <div className={`relative max-w-7xl mx-auto w-full min-h-screen flex items-center flex-col gap-10 justify-end pb-10`}>
+                        <div key={movie.id} className="flex items-center flex-col gap-10 max-w-7xl">
+                            <div className="relative group/play bg-black/50 rounded-md border border-gray-100 w-max h-max z-50">
+                                <img src={movie.capa} className="w-44 transition opacity-100 group-hover/play:opacity-40"/>
+                                <button
+                                    className="invisible absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border border-gray-100 bg-gray-200/20 rounded-full p-5 cursor-pointer transition hover:bg-gray-200/10 group-hover/play:visible"
+                                    type="button">
+                                    <FaPlay className="size-10" />
+                                </button>
+                            </div>
                             <ul className="flex gap-8">
-                                <li className="px-12 py-2 font-medium border border-gray-100 bg-gray-200/20 rounded-full">{movie.catagories[0]}</li>
-                                <li className="px-12 py-2 font-medium border border-gray-100 bg-gray-200/20 rounded-full">{movie.catagories[1]}</li>
-                                <li className="px-12 py-2 font-medium border border-gray-100 bg-gray-200/20 rounded-full">{movie.catagories[2]}</li>
+                                <li className="px-12 py-2 font-medium border border-gray-100 bg-gray-200/20 rounded-full select-none">{movie.catagories[0]}</li>
+                                <li className="px-12 py-2 font-medium border border-gray-100 bg-gray-200/20 rounded-full select-none">{movie.catagories[1]}</li>
+                                <li className="px-12 py-2 font-medium border border-gray-100 bg-gray-200/20 rounded-full select-none">{movie.catagories[2]}</li>
                             </ul>
                             <p>{movie.description}</p>
                         </div>
-                        <button 
-                            className="size-20 border border-gray-100 bg-gray-200/20 rounded-full flex justify-center items-center hover:bg-gray-200/10" 
+
+                        <button
+                            className="border border-gray-100 bg-gray-200/20 rounded-full p-5 hover:bg-gray-200/10 cursor-pointer transition-all"
                             type="button">
-                                <FaPlay className="size-10 -mr-1"/>
+                            <FaPlay className="size-10" />
+                        </button>
+
+                        <div className="absolute left-0 top-1/2 -translate-y-1/2 flex justify-between w-full px-6">
+                            <button className="transition-all hover:scale-105" type="button" title="Volta">
+                                <GrPrevious className="w-11 h-auto" />
                             </button>
-                    </section>
+                            <button className="transition-all hover:scale-105" type="button" title="Avançar">
+                                <GrNext className="w-11 h-auto" />
+                            </button>
+                        </div>
+                    </div>
                 ))
             }
-        </>
+        </section>
     )
 }
