48c58369a38b335caff9b15f4f6a46051d41f492
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-05-10 15:22:13 -0300

    update styles

--- src/components/pages/home/components/focus-movies.tsx
+++ src/components/pages/home/components/focus-movies.tsx
@@ -21,7 +21,7 @@ export function MoviesInFocus() {
     }, [])
 
     return (
-        <section className="bg-focus-movie">
+        <section className="relative bg-focus-movie before:absolute before:bottom-0 before:left-0 before:size-full before:bg-gradient-to-t before:from-black before:to-transparent">
             {movies &&
                 movies.map((movie) => (
                     <div className={`relative max-w-7xl mx-auto w-full min-h-screen flex items-center flex-col gap-10 justify-end pb-10`}>
@@ -29,9 +29,9 @@ export function MoviesInFocus() {
                             <div className="relative group/play bg-black/50 rounded-md border border-gray-100 w-max h-max z-50">
                                 <img src={movie.capa} className="w-44 transition opacity-100 group-hover/play:opacity-40"/>
                                 <button
-                                    className="invisible absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border border-gray-100 bg-gray-200/20 rounded-full p-5 cursor-pointer transition hover:bg-gray-200/10 group-hover/play:visible"
+                                    className="invisible absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border border-gray-100 bg-gray-200/20 rounded-full p-4 cursor-pointer transition hover:bg-gray-200/10 group-hover/play:visible"
                                     type="button">
-                                    <FaPlay className="size-10" />
+                                    <FaPlay className="size-10 ml-1 -mr-1" />
                                 </button>
                             </div>
                             <ul className="flex gap-8">
@@ -43,9 +43,9 @@ export function MoviesInFocus() {
                         </div>
 
                         <button
-                            className="border border-gray-100 bg-gray-200/20 rounded-full p-5 hover:bg-gray-200/10 cursor-pointer transition-all"
+                            className="border border-gray-100 bg-gray-200/20 rounded-full p-4 hover:bg-gray-200/10 cursor-pointer transition-all"
                             type="button">
-                            <FaPlay className="size-10" />
+                            <FaPlay className="size-10 ml-1 -mr-1" />
                         </button>
 
                         <div className="absolute left-0 top-1/2 -translate-y-1/2 flex justify-between w-full px-6">
