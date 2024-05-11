823eacb3cdfae42103a7213ff5703ab141659008
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-05-11 18:33:30 -0300

    create context to getting id

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,24 @@
+import { createContext, useState } from "react";
 import { Footer } from "./components/footer/footer";
 import { Header } from "./components/header/header";
 import { Home } from "./components/pages/home/homes";
 
+interface TIdContext {
+  imdbID?: string
+  setImdbID?: React.Dispatch<React.SetStateAction<string>>;
+}
+export const IdContext = createContext<TIdContext>({});
+
 export function App() {
+  const [imdbID, setImdbID] = useState("");
+  
   return (
     <div className="bg-gray-950 text-gray-100 min-h-screen font-inter tracking-wider">
-      <Header />
-      <Home />
-      <Footer />
+      <IdContext.Provider value={{imdbID, setImdbID}}>
+        <Header />
+        <Home />
+        <Footer />
+      </IdContext.Provider>
     </div>
   )
 }
