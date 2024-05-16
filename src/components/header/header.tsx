3c04d2b67d4f962e179af016860332ad6d0048b1
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-05-16 17:52:19 -0300

    update detect is not home page

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import { IoSearchOutline } from "react-icons/io5";
 
 export function Header() {
-    const { imdbID, setImdbID } = useContext(IdContext);
+    const { setImdbID } = useContext(IdContext);
     const { dataMoviesSeries, setDataMoviesSeries } = useContext(PageDataContext);
 
     function clickToBackPage() {
@@ -29,11 +29,13 @@ export function Header() {
         })
     }
 
+    console.log()
+
     return (
         <header
             className="absolute top-0 left-0 w-full p-6 flex justify-between z-50"
         >
-            {imdbID === "" && !dataMoviesSeries?.data
+            {window.location.pathname === "/"
                 ? <h1
                     className="text-gray-100 font-bold text-4xl">
                     <span className="text-red-600">
@@ -48,26 +50,26 @@ export function Header() {
                     <GrPrevious className="w-11 h-auto" />
                 </Link>
             }
-            <Link to="/search">
-                <form
-                    onSubmit={handleSubimtForm}
-                    autoComplete="off"
-                    method="get"
-                    className="relative text-gray-500"
-                >
+            <form
+                onSubmit={handleSubimtForm}
+                autoComplete="off"
+                method="get"
+                className="relative text-gray-400 backdrop-blur-sm"
+            >
+                <Link to="/search">
                     <label
                         htmlFor="search">
                         <IoSearchOutline className="absolute top-1/2 left-2 -translate-y-1/2 size-8 z-10" />
                     </label>
                     <input
-                        className="bg-gray-200/20 text-gray-100 outline-none border border-gray-500  focus:border-gray-100 focus:outline-none placeholder:text-gray-500 w-[400px] p-2 pl-12 rounded-full"
+                        className="bg-gray-200/20 text-gray-100 outline-none border border-gray-500  focus:border-gray-100 focus:outline-none placeholder:text-gray-400 w-[400px] p-2 pl-12 rounded-full"
                         type="search"
                         name="search"
                         id="search"
                         placeholder="Pesquisar..."
                     />
-                </form>
-            </Link>
+                </Link>
+            </form>
         </header>
     )
 }
