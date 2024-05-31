8fc9ab465d583368e31f23cc5fc6bab7d89729dd
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-05-31 18:47:28 -0300

    create testing for footer component

---
+++ src/components/footer/footer.test.tsx
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/react";
+import { screen } from "@testing-library/dom"
+import "@testing-library/jest-dom";
+import { Footer } from "./footer";
+
+describe("Footer", () => {
+    it("should render component", () => {
+        render(<Footer />)
+
+        expect(screen.getByText("Filmes e Series")).toBeInTheDocument();
+        expect(screen.getByText("Lançamentos")).toBeInTheDocument();
+    })
+})
