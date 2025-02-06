25f76832ef893d5eb4e8326a01571f1c407ef816
matheus360k <gomes.craft10@gmail.com>
2025-02-06 17:50:20 -0300

    remove: files from watch page

--- src/page/watch/components/icon.tsx
+++
@@ -1,7 +0,0 @@
-import { ComponentProps } from "react";
-
-interface PropsICon extends ComponentProps<"i"> {}
-
-export function Icon(props: PropsICon) {
-    return <i {...props} className="cursor-pointer text-2xl max-sm:text-lg"></i>
-}
