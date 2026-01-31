29d8abbb44af4b00d58626f6542a96354efc4209
matheus369k <gomes.craft10@gmail.com>
2026-01-31 17:14:28 -0300

    refactor(search-more-container/component): organizaition code

--- src/components/search-more-container.tsx
+++ src/components/search-more-container.tsx
@@ -1,39 +1,45 @@
-export function SearchMoreContainer({
-  children,
-  isFetching,
-  title,
-}: {
+export function SearchMoreContainer(props: {
   children?: React.ReactNode
   isFetching: boolean
   title: string
 }) {
-  return (
-    <section className='flex flex-col px-2 gap-5 pt-24 max-w-7xl mx-auto min-h-screen h-fit w-full'>
-      <span className='pl-3 border-l-4 border-l-red-600 rounded'>
-        <h2 className='font-bold capitalize text-4xl max-lg:text-2xl'>
-          {title}
-        </h2>
-      </span>
+  function RenderChildrenUI() {
+    const isExistChildren = !!props.children
 
-      {children && <>{children}</>}
+    if (isExistChildren) {
+      return props.children
+    }
 
-      {!children && !isFetching && (
-        <p
-          aria-label='not found'
-          className='capitalize text-center justify-self-normal text-zinc-500'
-        >
-          not found
-        </p>
-      )}
-
-      {isFetching && (
+    if (props.isFetching) {
+      return (
         <p
           aria-label='loading'
           className='capitalize text-center justify-self-normal text-zinc-500'
         >
           loading...
         </p>
-      )}
+      )
+    }
+
+    return (
+      <p
+        aria-label='not found'
+        className='capitalize text-center justify-self-normal text-zinc-500'
+      >
+        not found
+      </p>
+    )
+  }
+
+  return (
+    <section className='flex flex-col px-2 gap-5 pt-24 max-w-7xl mx-auto min-h-screen h-fit w-full'>
+      <span className='pl-3 border-l-4 border-l-red-600 rounded'>
+        <h2 className='font-bold capitalize text-4xl max-lg:text-2xl'>
+          {props.title}
+        </h2>
+      </span>
+
+      {RenderChildrenUI()}
     </section>
   )
 }
