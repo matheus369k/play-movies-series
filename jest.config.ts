6f40c11ccb147695d7fe7107f80233776d2c5dcd
matheus369k <gomes.craft10@gmail.com>
2026-01-31 17:57:07 -0300

    perf: implementation new compiler para running test jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,24 +3,53 @@ import type { Config } from '@jest/types'
 const config: Config.InitialOptions = {
   rootDir: '.',
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
-  collectCoverage: true,
+  collectCoverage: false,
   coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['/node_modules/'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/build/'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+    '<rootDir>/coverage/',
+  ],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+    '<rootDir>/coverage/',
+  ],
+  watchPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+    '<rootDir>/coverage/',
+  ],
   coverageProvider: 'v8',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/src/util/jestSetup.ts'],
-  preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': [
+      '@swc/jest',
+      {
+        jsc: {
+          baseUrl: '.',
+          paths: {
+            '@/*': ['src/*'],
+          },
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
+            decorators: true,
+          },
+          transform: { react: { runtime: 'automatic' } },
+        },
+      },
+    ],
   },
   testEnvironmentOptions: {
     url: 'http://localhost:3000/play-movies-series',
   },
   testTimeout: 10000,
+  maxWorkers: '50%',
 }
 
 export default config
