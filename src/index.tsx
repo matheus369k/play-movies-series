15068d88b39ae0a58804b6bc2a167800e2a6477b
matheus369k <gomes.craft10@gmail.com>
2026-03-23 17:31:17 -0300

    feat: add google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import { App } from './app'
 import 'react-multi-carousel/lib/styles.css'
 import './style/index.css'
 import React from 'react'
+import ReactGA from 'react-ga4'
+import { env } from './util/env'; 
+
+ReactGA.initialize(env.VITE_GA_ID);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
